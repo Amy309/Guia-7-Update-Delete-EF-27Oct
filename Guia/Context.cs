48ad4ad113be4bd893ac912587abd857c9d2d32a
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-10-27 19:47:05 -0600

    Relleno

---
+++ Guia/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia
+{
+    public class ContextDB : DbContext
+    {
+        public DbSet<Student> Estudiante { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-NFDMETJ\\SQLEXPRESS;Database=Progra2;Trusted_Connection=True;");
+
+        }
+    }
+}
